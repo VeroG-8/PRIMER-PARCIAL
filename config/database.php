cd95cf6e67e929b8319a0fc3ec58b4856af130cf
verog <verogdevs@gmail.com>
2026-09-03 00:21:31 -0300

    se anido correctamente

--- config/database.php
+++ config/database.php
@@ -1,5 +1,7 @@
 <?php
-$host="127.0.0.1"; $user="root"; $password=""; $database="DBLogin";
+$host="127.0.0.1";
+$user="root"; $password="";
+$database="DBLogin";
 $conexion=new mysqli($host,$user,$password,$database);
 if($conexion->connect_error){die("Error de conexión: ".$conexion->connect_error);}
 $conexion->set_charset("utf8mb4");
